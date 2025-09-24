ab3ef2f6cdd8cd463681f2f21831d69537c492c1
Sai Tejaswini Surisetti <126457241+sai-TEJA-saitej123@users.noreply.github.com>
2025-09-24 18:53:41 +0530

    made changes in hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,17 +3,17 @@ import heroDesign from "../asessts/images/HeroDesign.svg"
 const Hero: React.FC = () => {
     return (
         <>
-            <div className="flex home justify-around">
+            <div className="flex  justify-around gap-2 mt-10">
 
-                <div className="my-auto">
-                    <h2 className="font-semibold text-6xl leading-[5.4rem] mr-5">
+                <div className="my-auto mr-44 ">
+                    <h2 className="font-bold text-6xl leading-[5.4rem] mr-20">
                         Navigating the <br /> digital  landscape <br /> for success
                     </h2>
-                    <p className="text-[18px]">Our digital marketing agency helps businesses grow and <br /> succeed online through a range of services including SEO, <br /> PPC, social media marketing, and content creation.</p>
-                    <button className="p-3 w-[264px] h-[68px] text-[20px] mt-8 consultation">Book a consultation</button>
+                    <p className="text-[20px]  leading-7 mt-6">Our digital marketing agency helps businesses grow and <br /> succeed online through a range of services including SEO, <br /> PPC, social media marketing, and content creation.</p>
+                    <button className="p-2 w-[264px] h-[68px] text-[20px] mt-8 consultation bg-[#191a23] text-white  rounded-2xl semibold hover:bg-[#b9ff66] duration-100 ease-in-out border-black">Book a consultation</button>
                 </div>
                 <div >
-                    <img src={heroDesign} alt="" />
+                    <img src={heroDesign} alt="" className="w-[600px] h-[515px] " />
                 </div>
             </div>
         </>)
